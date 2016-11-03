af5f7ded821b891fcd50ae9e59ba8b8e519f17ad
ShMcK <shawn.j.mckay@gmail.com>
2016-11-03 14:38:33 -0700

    update pong-starter with working webpack, public directory, removed files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
-import './game.css';
+import './styles/game.css';
+import Game from './partials/Game'
 
-class Game {
-	constructor() {
-		const canvas = document.getElementById('game');
-	}
+// create a game instance
+const game = new Game('game');
+
+// game speed
+const ms = 30;
+
+function start() {
+  // recursive function creates a loop
+  (function gameLoop() {
+
+    // render game here
+
+    setTimeout(gameLoop, ms);
+  }());
 }
+
+start();
--- webpack.config.js
+++ webpack.config.js
@@ -11,51 +11,45 @@ const PATHS = {
 
 module.exports = {
 	entry: {
-		src: join(PATHS.src, 'index.js')
-	},
-	resolve: {
-		extensions: ['', '.js']
+		src: './src/index.js'
 	},
 	output: {
-		path: PATHS.build,
-		publicPath: '/pong/',
-		filename: 'bundle.js'
+		path: __dirname,
+		filename: 'build/bundle.js'
 	},
 	module: {
-		loaders: [{
-			test: /\.css$/,
-			loaders: ['style', 'css'],
-			include: PATHS.src
-		}, {
-			test: /\.js$/,
-			loader: 'babel',
-			include: PATHS.src,
-			exclude: /node_modules/
-		}, {
-			test: /\.(eot|svg|ttf|woff|woff2)$/,
-			include : PATHS.fonts,
-			loader: `file?name=/fonts/[name].[ext]`
-		}]
+		loaders: [
+			{
+				test: /\.css$/,
+				loaders: ['style', 'css'],
+				include: PATHS.src
+			}, {
+				test: /\.js$/,
+				loader: 'babel',
+				include: PATHS.src,
+				exclude: /node_modules/
+			}, {
+				test: /\.(eot|svg|ttf|woff|woff2)$/,
+				loader: 'file?name=public/fonts/[name].[ext]'
+			}
+		]
 	},
-	devtool: 'eval-source-map',
 	devServer: {
-		contentBase: process.cwd(),
-
-		historyApiFallback: true,
-		hot: true,
 		inline: true,
-		progress: true,
 
 		// display only errors to reduce the amount of output
 		stats: 'errors-only',
 
 		// parse host and port from env so this is easy
 		// to customize
-		host: process.env.HOST,
-		port: process.env.PORT
+		host: '0.0.0.0',
+		port: '3000',
+		watchOptions: {
+			aggregateTimeout: 300,
+			poll: true
+		}
 	},
 	plugins: [
-		new webpack.HotModuleReplacementPlugin(),
-		new OpenBrowserPlugin({ url: 'http://localhost:8080/webpack-dev-server/bundle'}),
+		new OpenBrowserPlugin({ url: 'http://localhost:3000/'}),
 	]
 };
