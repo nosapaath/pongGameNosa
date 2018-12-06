8a76c457342a265de0b740afb70a49b660f9c066
Nosa Paath <adepaath@gmail.com>
2018-12-05 19:03:18 -0800

    ball bouncing everything

--- src/partials/Ball.js
+++ src/partials/Ball.js
@@ -7,21 +7,38 @@ export class Ball{
     this.boardWidth = width; 
     this.boardHeight = height;
     this.spaceBar = space;
+    this.direction = 1;
+
     this.reset();
     document.addEventListener('keydown', event => {
       switch(event.key){
         case this.spaceBar:
-        // this.x = this.x + this.speed;
-        // this.y = this.y + this.speed;
-        this.y = Math.min(0, Math.floor(Math.random() * 10 - 5));
-        this.x = Math.max(this.boardHeight, this.ballDirection * (6 - Math.abs(this.y))); 
+        this.reset();
         break;
       }
     });
   }
+  wallCollision(){
+    const hitLeft = (this.x - this.radius <= 0); 
+    const hitRight = (this.x + this.radius >= this.boardWidth); 
+    const hitTop = (this.y - this.radius <= 0);
+    const hitBottom = (this.y + this.radius >= this.boardHeight ); 
+
+    if(hitTop || hitBottom){
+      this.vy *= -1;
+    } else if (hitLeft || hitRight){
+      this.vx *= -1;
+    }
+  }
+
   reset(){
     this.x = this.boardWidth/2;
     this.y = this.boardHeight/2;
+    this.vy = 0;
+    while (this.vy === 0){
+      this.vy = Math.floor(Math.random()* 10 - 5);
+    }
+    this.vx = this.direction * (6 - Math.abs(this.vy));
   }
   render(svg){
     let circle = document.createElementNS(SVG_NS, 'circle'); 
@@ -29,11 +46,11 @@ export class Ball{
     circle.setAttributeNS(null, 'cx', this.x);
     circle.setAttributeNS(null, 'cy', this.y);
     circle.setAttributeNS(null,'fill','white');
+    this.wallCollision();
+    this.x = this.x + this.vx; 
+    this.y = this.y + this.vy;
+
+    
     svg.appendChild(circle)
   }
 }
-
-// arigato
-// storing <- o ->
-// x speed and y speed 
-// 
